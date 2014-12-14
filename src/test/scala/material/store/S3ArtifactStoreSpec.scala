ba98c66ddd6cd8c3dad32607032e1d36f4a70c3c
Sriram <sriram@indix.com>
2014-12-14 20:37:34 +0530

    Artifact Store talks to S3 now with AWS SDK.

--- src/test/scala/material/store/S3ArtifactStoreSpec.scala
+++ src/test/scala/material/store/S3ArtifactStoreSpec.scala
@@ -3,14 +3,20 @@ package material.store
 import org.scalatest.FlatSpec
 import java.io.File
 import org.scalatest.matchers.ShouldMatchers._
+import org.mockito.Mockito._
+import com.amazonaws.services.s3.AmazonS3Client
+import org.mockito.Matchers._
+import com.amazonaws.services.s3.model.{ObjectMetadata, GetObjectRequest}
 
 class S3ArtifactStoreSpec extends FlatSpec {
-  object FileHelperUtil extends FileHelper
+  object StoreHelperUtil$ extends StoreHelper
 
   "S3ArtifactStore" should "get file from remote file path to local file path" in {
-    val store = S3ArtifactStore()
-    store.get(this.getClass.getResource("/remoteGetFile").getPath, "/tmp/localGetFile")
-    FileHelperUtil.exists("/tmp/localGetFile", store.conf) should be (true)
-    new File("/tmp/localGetFile").delete()
+    val s3Client = mock(classOf[AmazonS3Client])
+    val objectMetadata = new ObjectMetadata()
+    objectMetadata.setContentMD5("MD5")
+    when(s3Client.getObject(anyObject[GetObjectRequest](), anyObject[File]())).thenReturn(objectMetadata)
+    val store = S3ArtifactStoreWithS3Client(s3Client, "indix-bucket")
+    store.get("remoteFile", "localFile").asInstanceOf[CopySuccess].md5 should be("MD5")
   }
 }
